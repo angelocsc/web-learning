653dac2b32ca5896c930add346d3fc340196f9d5
Angelo Chou <angelo.csc@msa.hinet.net>
2017-12-08 09:26:34 +0800

    add more function to jquery

--- scripts/jqTest.js
+++ scripts/jqTest.js
@@ -1,13 +1,26 @@
+//wait for fullly load
 $(function(){
-	var showName = $("#showName")
-	function addContent(){
-		showName.append("new content");
-	}
+	var showName = $("#showName");
+	var name = $("#Name");
+	var input_form = $("#input_form");
+
+	name.hide(500).delay(100).show(500)
 
 	showName.on("click" , addContent);
+	function addContent(){
+		showName.addClass('special');
+	}
+
+	
 	$("#myButton").on("click" , remove);
 
 	function remove(){
-		showName.fadeToggle(200)
+		showName.fadeToggle(200);
+	}
+	
+	$(".button").on("click",fadeEffect);
+	function fadeEffect(){
+		var panelID = $(this).attr('data-panel');
+		$("#"+panelID).addClass('special');
 	}
 });
